a7094c3978b5361dcdf1f9dd9e38602eea266dc1
Peter Kasik <peter.kasik@anz.com>
2018-10-16 17:04:35 +0800

    Backgrounds

--- js/chowello.js
+++ js/chowello.js
@@ -1,3 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-  window.scrollTo(0, 100);
-}, false);
+$(function () {
+  $("body").vegas({
+    timer: false,
+    cover: true,
+    delay: 5000,
+    slides: [
+      { src: "img/guacamole.jpg" },
+      { src: "img/corn.jpg" },
+      { src: "img/carrots.jpg" },
+      { src: "img/eggs.jpg" },
+      { src: "img/coffee.jpg" },
+      { src: "img/fruits.jpg" },
+      { src: "img/smoothies.jpg" }
+    ]
+  });
+});
+
+
